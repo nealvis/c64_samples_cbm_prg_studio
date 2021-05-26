8b424abc32262170b0e240e4b8f1fbe990f89fb4
Neal Smith <nealvis@gmail.com>
2021-05-26 06:14:31 -0700

    added direct to screen as well in addition to calling basic routine

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,6 +1,13 @@
-; 10 SYS (4096)
+; This sample shows two ways to print to the screen
+; 1 Calling a routine in BASIC
+; 2 Writing direct to screen memory
 
-*=$0801
+
+
+*=$0801 ; location to put 1 line basic program so we can just
+        ; type run to execute the assembled program.
+        ; will just call assembled program at correct location
+        ;    10 SYS (4096)
 
         ; These bytes are a one line basic program that will 
         ; do a sys call to assembly language portion of
@@ -18,17 +25,46 @@ CLEAR_SCREEN_KERNAL = $E544     ; Kernal routine to clear screen
 PRINT_STRING_BASIC = $AB1E      ; Basic routine to print text
 
 StrToPrint
-        null "HELLO WORLD"     ; null terminated string to print
+        null "HELLO VIA BASIC"  ; null terminated string to print
+                                ; via the BASIC routine
 
+StrToPoke
+        null 'hello direct'     ; null terminated string to print
+                                ; via copy direct to screen memory
+
+
+SCREEN_START = $0400            ; The start of c64 screen memory
+
+; we'll write directly to screen starting at a somewhat random
+; screen location 
+SCREEN_DIRECT_START = SCREEN_START + $0100; 
+
+
+; our assembly code will goto this address
 *=$1000
         
 Main
-        jsr CLEAR_SCREEN_KERNAL ; clear screeen leave cursor upper left
-        
 
-        lda #<StrToPrint        ; lsB of addr of string to print to A
-        ldy #>StrToPrint        ; msB of addr of str to print to Y
+        ; clear screeen leave cursor upper left
+        jsr CLEAR_SCREEN_KERNAL 
+        
+        ; method 1 call basic routine since we cleared screen 
+        ; above the string will start in upper left
+        lda #<StrToPrint        ; LSB of addr of string to print to A
+        ldy #>StrToPrint        ; MSB of addr of str to print to Y
         jsr PRINT_STRING_BASIC  ; call kernal routine to print the string
+
+
+        ; method 2 write direct to screen memory
+        ldx #0                  ; use x reg as loop index start at 0
+DirectLoop
+        lda StrToPoke,x         ; put a byte from string into accum
+        beq Done                ; if the byte was 0 then we're done 
+        sta SCREEN_DIRECT_START,x ; Store the byte to screen
+        inx                     ; inc to next byte and next screen location 
+        jmp DirectLoop          ; Go back for next byte
+Done
+        
 
         rts                     ; program done, return
 
