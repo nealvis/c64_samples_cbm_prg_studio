122753d4292e28a6cc1a028024cd68898fbe78d1
Neal Smith <nealvis@gmail.com>
2021-05-27 07:49:40 -0700

    cleaned up code and making multiple calls to each macro

--- macros/macros.asm
+++ macros/macros.asm
@@ -20,7 +20,7 @@
         BYTE $00, $00, $00      ; end of basic program (addr $080E from above)
 
 
-
+; address of the Kernal routine to clear the screen
 CLEAR_SCREEN_KERNAL = $E544
 
 ; macro with no parameters to clear screen and 
@@ -31,60 +31,69 @@ defm clear_screen_mac
 
 
 
-; Address of Kernal routine to clear screen
+; Address of BASIC routine to print a string at 
+; current cursor location
 PRINT_STRING_BASIC = $AB1E      
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; macro to print a null terminated string via basic routine at 
 ; current cursor
 ; /1 is the address of the first char of string to print
 defm print_string_basic_mac
-        lda #</1        ; LSB of addr of string to print to A
-        ldy #>/1        ; MSB of addr of str to print to Y
+        lda #</1                ; LSB of addr of string to print to A
+        ldy #>/1                ; MSB of addr of str to print to Y
         jsr PRINT_STRING_BASIC  ; call kernal routine to print the string
         endm
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-SCREEN_COLS = 40
-SCREEN_ROWS = 25
-SCREEN_START_ADDR = $0500 ;SCREEN_START + (/1 * 40) + (/2)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; macro to print a string by writing directly to screen memory
-; /1 is screen column (x position) to start
-; /2 is screen row (y position) to start
-; /3 is the address of the first char of string to print
-defm print_string_direct_mac
-        ldx #0                  ; use x reg as loop index start at 0
-StartAddr = SCREEN_START + (/2 * 40) + /1
-_DirectLoop
-        lda /3,x         ; put a byte from string into accum
-        beq _Done                ; if the byte was 0 then we're done 
-        sta StartAddr,x ; Store the byte to screen
-        inx                     ; inc to next byte and next screen location 
-        jmp _DirectLoop          ; Go back for next byte
-_Done
-        endm
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-StrToPrint
-        null "HELLO VIA BASIC"  ; null terminated string to print
-                                ; via the BASIC routine
-
-StrToPoke
-        null 'hello direct'     ; null terminated string to print
-                                ; via copy direct to screen memory
-
-
+; Some constants for screen 
 SCREEN_START = $0400            ; The start of c64 screen memory
-
-; we'll write directly to screen starting at a somewhat random
-; screen location 
-;SCREEN_DIRECT_START = SCREEN_START + $0100; 
+SCREEN_COLS = 40                ; chars across
+SCREEN_ROWS = 25                ; chars down
 
 
-; our assembly code will goto this address
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; macro to print a string by writing directly to screen memory
+; /1 is the screen location for first character of string
+; /2 is the address of the first char of string to print
+defm print_string_direct_mac
+        ldx #0          ; use x reg as loop index start at 0
+@DirectLoop
+        lda /2,x        ; put a byte from string into accum
+        beq @Done       ; if the byte was 0 then we're done 
+        sta /1,x        ; Store the byte to screen
+        inx             ; inc X to next byte and next screen location 
+        jmp @DirectLoop ; Loop back to write next byte
+@Done
+        endm
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; some data for our program.  This will be directly after
+; the sys(4096) BASIC program but before the start of 
+; the assembly program below
+
+; null terminated strings to print via basic routine
+StrHelloBasic
+        null "HELLO VIA BASIC   "  
+
+StrGoodbyeBasic
+        null "GOODBYE VIA BASIC"
+           
+
+; null terminated strings to print direct to screen memory
+StrHelloDirect
+        null 'hello direct'
+
+StrGoodbyeDirect
+        null 'goodbye direct'
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+; our assembly code will start at this address
 *=$1000
         
 Main
@@ -93,10 +102,21 @@ Main
         clear_screen_mac
         
         ; print a string via basic routine
-        print_string_basic_mac StrToPrint
+        print_string_basic_mac StrHelloBasic
 
-        ; print a string by writing to screen memory
-        print_string_direct_mac 5, 5, StrToPoke
+        ; print a string starting at x=5, y=5 
+        ; by writing to screen memory
+screen_addr1 = SCREEN_START + (5 * SCREEN_COLS) + 5
+        print_string_direct_mac screen_addr1, StrHelloDirect
+
+        ; print a string via basic routine
+        print_string_basic_mac StrGoodByeBasic
+
+        ; print a string starting at to x=6, y=6 
+        ; by writing to screen memory
+screen_addr2 = SCREEN_START + (6 * SCREEN_COLS) + 6
+        print_string_direct_mac screen_addr2, StrGoodbyeDirect
+
 
         ; program done, return
         rts                     
